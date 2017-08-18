bf8ecb033cb694f499e880dfca80db1eaf7e8f24
张振勇 <zhangzy@missfresh.cn>
2017-08-18 10:16:22 +0800

    init

---
+++ dist/js/001.js
@@ -0,0 +1,1 @@
+console.log(1111111);var test=function(){console.log(111122222)};window.onload=function(){test()};
---
+++ js/001.js
@@ -0,0 +1,7 @@
+console.log(1111111)
+var test = function () {
+  console.log(111122222)
+}
+window.onload = function () {
+  test()
+}
