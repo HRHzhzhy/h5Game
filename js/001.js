a4fe351f6323b2b79ab2a086e434d39c219b8c49
张振勇 <zhangzy@missfresh.cn>
2017-08-24 15:09:27 +0800

    ignore dist

--- js/001.js
+++ js/001.js
@@ -3,5 +3,33 @@ var test = function () {
   console.log(111122222)
 }
 window.onload = function () {
-  test()
+  var log = console.log.bind(console)
+  var canvas = document.querySelector('#canvas')
+  var context = canvas.getContext('2d')
+  var x = 100
+  var y = 120
+  var img = new Image()
+  img.src = '../images/paddle.png'
+  img.onload = function () {
+    context.drawImage(img, x, y, 100, 10)
+  }
+  window.addEventListener('keydown', function (event) {
+    var k = event.key
+    if (k == 'a') {
+      x -= 10
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      context.drawImage(img, x, y, 100, 10)
+    }
+    if (k == 'd') {
+      x += 10
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      context.drawImage(img, x, y, 100, 10)
+    }
+  })
+  // setInterval(function(){
+  //     // update x and y
+  //     // draw
+  //     context.clearRect(0, 0, canvas.width, canvas.height)
+  //     context.drawImage(img, x, y, 100, 10)
+  // }, 1000/30)
 }
